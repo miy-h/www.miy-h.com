7d0d1aee06ef64acd191f7a0fa1840905d727431
MIYAGAKI Hinata <mail@miy-h.com>
2025-01-27 15:49:16 +0900

    Introduce i18n plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,15 @@
+import { EleventyI18nPlugin } from "@11ty/eleventy";
+
+function fullUrl(url, base = "https://www.miy-h.com/") {
+  return new URL(url, base).href;
+}
+
 export default async function(eleventyConfig) {
-	// Configure Eleventy
+  eleventyConfig.addGlobalData("layout", "layout.njk");
+  eleventyConfig.addFilter("fullUrl", fullUrl);
+	eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "en"
+  });
 };
 
 export const config = {
